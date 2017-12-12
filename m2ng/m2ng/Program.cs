3ae4d3abbc055e97c6f9a7b0b687135b964b3b6c
Katrin <katrin.kirotarp@gmail.com>
2017-12-12 20:07:01 +0200

    fix

--- m2ng/m2ng/Program.cs
+++ m2ng/m2ng/Program.cs
@@ -15,22 +15,9 @@ namespace m2ng
             path = @"../../../Invetory.txt";
             File.Delete(path);
             //Ärkamisest telefonikõnele vastamiseni
-            // dialoog.Dialo();
+            dialoog.Dialo();
             //Telefonikõnest külani
-<<<<<<< HEAD
-            // lennujaam.Lennujaam();
-
-            TextFile majake = new TextFile(@"C:\code\Katrin\Tekstim2ng\m2ng\m2ng\info\ascii_maja.txt");
-            Console.WriteLine(majake.getText());
-
-            Console.ReadLine();
-
-            
-=======
             lennujaam.Lennujaam();
-            //Koobas, hiljem storyga kokku sobitada. Lõpetada samuti ( võitlus ).
-            //Koobas.Kooba();
->>>>>>> 412c3f991aff4b23d0ad37e4d2ee11f5677ab007
         }
     }
 }
