d1f0082e2adabe2122cf9ccf9a415f05ed45d96c
Katrin <33371336+progejaKatrin@users.noreply.github.com>
2017-12-14 09:12:00 +0200

    raha

--- m2ng/m2ng/Program.cs
+++ m2ng/m2ng/Program.cs
@@ -14,43 +14,61 @@ namespace m2ng
         public static string path;
         static void Main(string[] args)
         {
+            
+            //path = @"../../../Invetory.txt";
+            //File.Delete(path);
 
-            path = @"../../../Invetory.txt";
-            File.Delete(path);
 
-
-            //ASCII
+            ////ASCII
             //string pilt = System.IO.File.ReadAllText(@"C:\Users\opilane\Documents\GitHub\Tekstim2ng\pilt.txt");
             //Console.WriteLine(pilt);
 
-            //Ärkamisest telefonikõnele vastamiseni
-            dialoog.Dialo();
-            //Telefonikõnest külani
-             lennujaam.Lennujaam();
+            ////Ärkamisest telefonikõnele vastamiseni
+            //dialoog.Dialo();
+            ////Telefonikõnest külani
+            // lennujaam.Lennujaam();
 
-            //Lennujaamas sees ning linnas:
-            linna.linnas();
-            //Linnas. Alates teisest karakterist.
-            linnas.linn();
-            //Apteek
-            apteek.apt();
+            ////Lennujaamas sees ning linnas:
+            //linna.linnas();
+            ////Linnas. Alates teisest karakterist.
+            //linnas.linn();
+            ////Apteek
+            //apteek.apt();
 
-            //Pood
-            //Console.WriteLine("PATH : {0}", Directory.GetCurrentDirectory());
+            ////Pood
+            ////Console.WriteLine("PATH : {0}", Directory.GetCurrentDirectory());
             
-            TextFile ascii = new TextFile(@"..\..\info\ascii.txt");
-            Console.WriteLine(ascii.getText());
-            //Apteek ja rohud
+            //TextFile ascii = new TextFile(@"..\..\info\ascii.txt");
+            //Console.WriteLine(ascii.getText());
+            ////Apteek ja rohud
             
-            TextFile aptjek = new TextFile(@"..\..\info\aptjek.txt");
-            Console.WriteLine(aptjek.getText());
+            //TextFile aptjek = new TextFile(@"..\..\info\aptjek.txt");
+            //Console.WriteLine(aptjek.getText());
+
+            //Console.ReadLine();
+
+
+            //raha
+            Console.WriteLine("Hetke saldo : {0}", raha.Saldo());
+            //kui soovid raha juurde lisada
+            raha.Muuda(10.3F);
+
+            Console.WriteLine("Hetke saldo : {0}", raha.Saldo());
+            bool jaak = false;
+            do
+            {
+                //kui soovid raha ära võtta
+                jaak = raha.Muuda(-5.0F);
+                Console.WriteLine("Veel on raha : {0}", raha.Saldo());
+
+
+            } while (jaak);
 
             Console.ReadLine();
-
         }
 
 
 
-        }
+    }
     }
 
